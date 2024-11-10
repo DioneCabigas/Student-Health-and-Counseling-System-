60b121dfe86343a3e25fb25642dfa384b2ec4a5f
Dione Alfred Cabigas <143990611+DioneCabigas@users.noreply.github.com>
2024-11-11 07:51:47 +0800

    Delete Student Health and Counseling System directory

--- Student Health and Counseling System/StudHealthCouncelingSys/projectApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard_view),
-    path('index.html', views.dashboard_view),
-    path('appointment.html', views.appointment_view),
-    path('appointmentAddPatient.html', views.appointment_scheduling_view)
-]
